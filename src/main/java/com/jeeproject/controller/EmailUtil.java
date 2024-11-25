d695efb4d433cad8516690bd57c9269a221de1e2
MathApc <84502718+MathApc@users.noreply.github.com>
2024-11-25 12:45:36 +0100

    ajout email

---
+++ src/main/java/com/jeeproject/controller/EmailUtil.java
@@ -0,0 +1,51 @@
+à ajouter dans le pom.xml 
+<dependency>
+    <groupId>javax.mail</groupId>
+    <artifactId>javax.mail-api</artifactId>
+    <version>1.6.2</version>
+</dependency>
+<dependency>
+    <groupId>com.sun.mail</groupId>
+    <artifactId>javax.mail</artifactId>
+    <version>1.6.2</version>
+</dependency>
+
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Properties;
+
+public class EmailUtil {
+    private static final String SMTP_HOST = "smtp.gmail.com";
+    private static final int SMTP_PORT = 587;
+    private static final String SMTP_USERNAME = "votre_email@gmail.com";
+    private static final String SMTP_PASSWORD = "votre_mot_de_passe";
+
+    public static void sendEmail(String to, String subject, String messageContent) throws MessagingException {
+        // Configurer les propriétés du serveur SMTP
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", SMTP_HOST);
+        properties.put("mail.smtp.port", SMTP_PORT);
+
+        // Créer une session
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(SMTP_USERNAME, SMTP_PASSWORD);
+            }
+        });
+
+        // Créer un message
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(SMTP_USERNAME));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+        message.setSubject(subject);
+        message.setText(messageContent);
+
+        // Envoyer le message
+        Transport.send(message);
+    }
+}
+
